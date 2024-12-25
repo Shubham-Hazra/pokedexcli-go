9ba1633182e061e1437a43244bd83ebe25c4322e
Shubham-Hazra <shubhamhazra1234@gmail.com>
2024-12-25 15:12:53 +0530

    Added explore

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ const (
 	cacheInterval = 15 * time.Second
 )
 
-type Config struct {
+type LocationAreaConfig struct {
 	Next     string `json:"next"`
 	Previous string `json:"previous"`
 	Results  []struct {
@@ -28,10 +28,19 @@ type Config struct {
 	} `json:"results"`
 }
 
+type LocationAreaResponse struct {
+	PokemonEncounters []struct {
+		Pokemon struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"pokemon"`
+	} `json:"pokemon_encounters"`
+}
+
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(args []string) error
 }
 
 func fetchData(cache *pokecache.Cache, url string, target interface{}) error {
@@ -59,85 +68,131 @@ func fetchData(cache *pokecache.Cache, url string, target interface{}) error {
 	return json.Unmarshal(body, target)
 }
 
-func commandExit() error {
+type cli struct {
+	cache  *pokecache.Cache
+	LocationAreaConfig *LocationAreaConfig
+}
+
+func (c *cli) commandExit(args []string) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp(commands *map[string]cliCommand) error {
+func (c *cli) commandHelp(args []string) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Printf("Usage:\n\n")
-	for _, cmd := range *commands {
+	for _, cmd := range getCommands(c) {
 		fmt.Printf("%v: %v\n", cmd.name, cmd.description)
 	}
 	return nil
 }
 
-func commandMap(cache *pokecache.Cache, config *Config) error {
-	if config.Next == "" {
+func (c *cli) commandMap(args []string) error {
+	if c.LocationAreaConfig.Next == "" {
 		fmt.Println("No more pages to load.")
 		return nil
 	}
 
-	err := fetchData(cache, config.Next, config)
+	err := fetchData(c.cache, c.LocationAreaConfig.Next, c.LocationAreaConfig)
 	if err != nil {
 		return err
 	}
 
-	for _, loc := range config.Results {
+	for _, loc := range c.LocationAreaConfig.Results {
 		fmt.Println(loc.Name)
 	}
 	return nil
 }
 
-func commandMapb(cache *pokecache.Cache, config *Config) error {
-	if config.Previous == "" {
+func (c *cli) commandMapb(args []string) error {
+	if c.LocationAreaConfig.Previous == "" {
 		fmt.Println("No previous page available.")
 		return nil
 	}
 
-	err := fetchData(cache, config.Previous, config)
+	err := fetchData(c.cache, c.LocationAreaConfig.Previous, c.LocationAreaConfig)
 	if err != nil {
 		return err
 	}
 
-	for _, loc := range config.Results {
+	for _, loc := range c.LocationAreaConfig.Results {
 		fmt.Println(loc.Name)
 	}
 	return nil
 }
 
-func main() {
-	cache := pokecache.NewCache(cacheInterval)
-	location_area_config := Config{
-		Next:     baseAPIURL + "/location-area/?offset=20&limit=20",
-		Previous: "",
+func (c *cli) commandExplore(args []string) error {
+	if len(args) < 1 {
+		return fmt.Errorf("please provide a location area name")
 	}
 
-	var commands map[string]cliCommand
-	commands = map[string]cliCommand{
+	locationArea := args[0]
+	url := fmt.Sprintf("%s/location-area/%s", baseAPIURL, locationArea)
+	var response LocationAreaResponse
+	err := fetchData(c.cache, url, &response)
+	if err != nil {
+		return fmt.Errorf("error exploring location area: %w", err)
+	}
+
+	fmt.Printf("Exploring %s...\n", locationArea)
+	fmt.Println("Found Pokemon:")
+
+	seenPokemon := make(map[string]bool)
+
+	for _, encounter := range response.PokemonEncounters {
+		if !seenPokemon[encounter.Pokemon.Name] {
+			fmt.Printf(" - %s\n", encounter.Pokemon.Name)
+			seenPokemon[encounter.Pokemon.Name] = true
+		}
+	}
+
+	return nil
+}
+
+func getCommands(c *cli) map[string]cliCommand {
+	return map[string]cliCommand{
 		"help": {
 			name:        "help",
 			description: "Displays a help message",
-			callback:    func () error {return commandHelp(&commands)},
+			callback:    c.commandHelp,
 		},
 		"exit": {
 			name:        "exit",
 			description: "Exit the Pokedex",
-			callback:    commandExit,
+			callback:    c.commandExit,
 		},
 		"map": {
 			name:        "map",
 			description: "Show the next page of locations",
-			callback:    func () error {return commandMap(&cache, &location_area_config)},
+			callback:    c.commandMap,
 		},
 		"mapb": {
 			name:        "mapb",
 			description: "Show the previous page of locations",
-			callback:    func () error {return commandMapb(&cache, &location_area_config)},
+			callback:    c.commandMapb,
+		},
+		"explore": {
+			name:        "explore",
+			description: "Explore a location area",
+			callback:    c.commandExplore,
 		},
 	}
+}
+
+func main() {
+	cache := pokecache.NewCache(cacheInterval)
+	LocationAreaConfig := LocationAreaConfig{
+		Next:     baseAPIURL + "/location-area/?offset=20&limit=20",
+		Previous: "",
+	}
+
+	c := &cli{
+		cache:  &cache,
+		LocationAreaConfig: &LocationAreaConfig,
+	}
+
+	commands := getCommands(c)
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
@@ -147,20 +202,22 @@ func main() {
 			break
 		}
 		userInput := scanner.Text()
-		cleanedInput := utils.CleanInput(userInput)
+		inputParts := utils.CleanInput(userInput)
 
-		if len(cleanedInput) < 1 {
+		if len(inputParts) < 1 {
 			fmt.Println("Please enter a command.")
 			continue
 		}
 
-		cmd := cleanedInput[0]
-		if command, ok := commands[cmd]; ok {
-			if err := command.callback(); err != nil {
+		cmdName := inputParts[0]
+		args := inputParts[1:]
+
+		if command, ok := commands[cmdName]; ok {
+			if err := command.callback(args); err != nil {
 				fmt.Printf("Encountered an error: %v\n", err)
 			}
 		} else {
-			fmt.Printf("Unsupported command: %v\n", cmd)
+			fmt.Printf("Unsupported command: %v\n", cmdName)
 		}
 	}
 }
